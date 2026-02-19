c66f2d8a5bdc203a6bb7e18dde8dd0ab802e5217
OrasanuIoana <80913904+OrasanuIoana@users.noreply.github.com>
2026-02-19 17:15:09 +0100

    Fixed MacOS certificate issue

--- main.py
+++ main.py
@@ -1,41 +1,51 @@
 import torch
 from torch.utils.data import Dataset
-from torchvision import datasets
+from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 import numpy as np
+import ssl
+import certifi
+import platform
 
-training_data = datasets.FashionMNIST(
-    root="data",
+
+if platform.system() == "Darwin":
+    print("Code running on MacOS. Fixing SSL Certificate.")
+    ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=certifi.where())
+    print("SSL Certificate fixed!")
+
+train_data = datasets.MNIST(
+    root="./data",
     train=True,
     download=True,
     transform=ToTensor()
 )
 
-test_data = datasets.FashionMNIST(
-    root="data",
+test_data = datasets.MNIST(
+    root="./data",
     train=False,
     download=True,
     transform=ToTensor()
 )
 
 labels_map = {
-    0: "T-Shirt",
-    1: "Trouser",
-    2: "Pullover",
-    3: "Dress",
-    4: "Coat",
-    5: "Sandal",
-    6: "Shirt",
-    7: "Sneaker",
-    8: "Bag",
-    9: "Ankle Boot",
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
 }
+
 figure = plt.figure(figsize=(8, 8))
 cols, rows = 3, 3
 for i in range(1, cols * rows + 1):
-    sample_idx = torch.randint(len(training_data), size=(1,)).item()
-    img, label = training_data[sample_idx]
+    sample_idx = torch.randint(len(train_data), size=(1,)).item()
+    img, label = train_data[sample_idx]
     figure.add_subplot(rows, cols, i)
     plt.title(labels_map[label])
     plt.axis("off")
